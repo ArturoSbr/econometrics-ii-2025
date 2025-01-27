07aff9f4e6caa591eddddcbc9ef6f125b1f3a123
ArturoSbr <soberonarturo@gmail.com>
2025-01-26 22:00:40 -0600

    Correct endog variable

--- assignments/rct/code/test-rct.py
+++ assignments/rct/code/test-rct.py
@@ -1,13 +1,13 @@
 # Imports
 import os, sys
 import importlib.util
-from linearmodels.panel.results import PanelEffectsResults
+from statsmodels.regression.linear_model import RegressionResultsWrapper
 
 # Run student's script
-student_path = os.path.join('.', 'test.py')
+student_path = os.path.join('.', 'rct.py')
 spec = importlib.util.spec_from_file_location('student_model', student_path)
 student_module = importlib.util.module_from_spec(spec)
-sys.modules["student_model"] = student_module
+sys.modules['student_model'] = student_module
 spec.loader.exec_module(student_module)
 
 # Test 1: declare model
@@ -18,15 +18,15 @@ except AttributeError:
     exit(1)
 
 # Test 2: Assert type
-msg2 = "Object `model` should be a 'PanelEffectsResult' instance, not {}."
-if not isinstance(model, PanelEffectsResults):
+msg2 = 'Object model should be a RegressionResultsWrapper instance, not {}.'
+if not isinstance(model, RegressionResultsWrapper):
     print(msg2.format(type(model)))
     exit(2)
 
 # Test 3: Assert estimates
 expected_value = 1.3375465850237276
 try:
-    msg3 = "Estimate for 'treat' is {}, expected {}."
+    msg3 = "Estimate for 'dark' is {}, expected {}."
     actual_value = model.params['treat'].item()
     assert abs(actual_value - expected_value) < 1e-6, (
         msg3.format(actual_value, expected_value)
--- assignments/rct/data/dict.md
+++ assignments/rct/data/dict.md
@@ -1,11 +1,12 @@
 # A/B Testing Dataset
 This is a synthetic dataset that simulates a Randomized Controlled Trial (RCT).
-In this imaginary scenario, suppose that an online retail store wants to answer
-the question _do customers spend more time browsing our site if its background
-color is dark?_
+In this imaginary scenario, a UK retailer wants to know if switching their
+site's theme from light to dark mode improves user engagement. Namely, the
+retailer randomly assigned the themes to users and wants to compare the
+conversion rate of both groups.
 
 > Your job is to estimate the causal effect of switching the site's background
-color from white (control) to black (treatment).
+color from white (control) to black (treatment) on users' conversion rate.
 
 ## Columns
 - User ID: Identifier for each user.
@@ -13,7 +14,7 @@ color from white (control) to black (treatment).
 - Page Views: Number of pages the user viewed during their session.
 - Time Spent: The total amount of time, in seconds, that the user spent on the
 site during the session.
-- Conversion: Indicates whether a user has completed a desired action (Yes/No).
+- Conversion: Indicates whether a user purchased an item during their session.
 - Device: Type of device used to access the website.
 - Location: The country in UK where the user is based in.
 
