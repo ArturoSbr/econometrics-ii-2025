dbdccb12134032be5a053a21f8923a091bc5f7d6
aleong92 <alvaro.leong92@gmail.com>
2025-03-09 20:25:49 -0600

    Refactor of eda notebook

---
+++ assignments/rct/code/rct-133463.py
@@ -0,0 +1,66 @@
+import os
+import pandas as pd
+import statsmodels.api as sm
+
+# Define PATH and read data
+PATH = os.path.join('..', 'data', 'raw.csv')
+df = pd.read_csv(PATH)
+
+# Rename columns
+df.columns = ['id', 'dark', 'views', 'time', 'purchase', 'mobile', 'location']
+
+# Map columns to numeric dtypes
+df.replace(
+    to_replace={
+        'dark': {'A': '0', 'B': '1'},
+        'mobile': {'Mobile': '1', 'Desktop': '0'},
+        'purchase': {'No': '0', 'Yes': '1'},
+        'location': {'Northern Ireland': 'Ireland'}
+    },
+    inplace=True
+)
+
+# Convert strings -> ints
+df[['dark', 'mobile', 'purchase']] = df[['dark', 'mobile', 'purchase']].astype(int)
+
+# Set `location`` to lowercase
+df['location'] = df['location'].str.lower()
+
+# One-hot encoding
+df = pd.get_dummies(
+    data=df,
+    prefix='',
+    prefix_sep='',
+    columns=['location'],
+    dtype=int
+)
+
+# Interaction
+df['dark_mobile'] = df['dark'].multiply(df['mobile'])
+
+# Constant
+df['const'] = 1
+
+# Declare specification
+spec = sm.OLS(
+    endog=df['purchase'],
+    exog=df[['const', 'ireland', 'scotland', 'wales', 'dark', 'dark_mobile']],
+    hasconst=True
+)
+
+# Fit model
+model = spec.fit()
+
+# Declare model
+spec2 = sm.OLS(
+    endog=df['purchase'],
+    exog=df[['const', 'ireland', 'scotland', 'wales', 'dark']],  # No interaction
+    hasconst=True
+)
+
+# Fit model
+model2 = spec2.fit()
+
+# View results
+print(model.summary())
+print(model2.summary())
