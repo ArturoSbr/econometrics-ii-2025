c1d74aa33b403645305492a07cfb0f9ddd09eba6
DaveVzHr <dvazqu26@itam.mx>
2025-03-11 07:45:26 -0600

    Update rct-166427.py

--- assignments/rct/code/rct-166427.py
+++ assignments/rct/code/rct-166427.py
@@ -1,1 +1,50 @@
+# Imports
+import os
+import pandas as pd
+import statsmodels.api as sm
 
+# Load data
+PATH = os.path.join('..', 'data', 'raw.csv')
+df = pd.read_csv(PATH)
+
+# Rename columns
+df.columns = ['id', 'dark', 'views', 'time', 'purchase', 'mobile', 'location']
+
+# Map columns to numeric dtypes
+df.replace(
+    to_replace={
+        'dark': {'A': '0', 'B': '1'},
+        'mobile': {'Mobile': '1', 'Desktop': '0'},
+        'purchase': {'No': '0', 'Yes': '1'},
+        'location': {'Northern Ireland': 'Ireland'}
+    },
+    inplace=True
+)
+
+# Convert strings -> ints
+df[['dark', 'mobile', 'purchase']] = df[['dark', 'mobile', 'purchase']].astype(int)
+
+# Set `location`` to lowercase
+df['location'] = df['location'].str.lower()
+
+# One-hot encoding
+df = pd.get_dummies(
+    data=df,
+    prefix='',
+    prefix_sep='',
+    columns=['location'],
+    dtype=int
+)
+
+# Constant
+df['const'] = 1
+
+# Declare model
+spec = sm.OLS(
+    endog=df['purchase'],
+    exog=df[['const', 'ireland', 'scotland', 'wales', 'dark']],
+    hasconst=True
+)
+
+# Fit model
+model = spec.fit()
