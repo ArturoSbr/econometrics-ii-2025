1c6595e8637c2ea58fb8eafa4b5be320ee807b0b
Frank <francisco.puerto123@gmail.com>
2025-03-11 18:09:26 -0600

    Nuevo intento

--- assignments/rct/code/rct-157104.py
+++ assignments/rct/code/rct-157104.py
@@ -1,48 +1,69 @@
 # Imports
 import os
+import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 
 # Load data
-PATH = os.path.join('..', 'data', 'raw.csv')
-df = pd.read_csv(PATH)
+PATH=os.path.join('..', 'data', 'raw.csv')
+df=pd.read_csv(PATH)
 
     # Rename columns
-df.columns = ['id', 'dark', 'views', 'time', 'purchase', 'mobile', 'location']
+df.columns=['id', 'dark', 'views', 'time', 'purchase', 'mobile', 'location']
 
 # Map columns to numeric types
 df.replace(
     to_replace={
-        'dark':{'A':'0', 'B':'1'},
-        'mobile':{'Mobile':'1', 'Desktop':'0'},
-        'purchase':{'No':'0', 'Yes':'1'},
-        'location':{'Northern Ireland':'Ireland'}
+        'dark': {'A': '0', 'B': '1'},
+        'mobile': {'Mobile': '1', 'Desktop': '0'},
+        'purchase': {'No': '0', 'Yes': '1'},
+        'location': {'Northern Ireland': 'Ireland'}
     },
     inplace=True
 )
 
 # Convert strings -> ints
-df[['dark', 'mobile', 'purchase']] = df[['dark', 'mobile', 'purchase']].astype(int)
+df[['dark', 'mobile', 'purchase']]=df[['dark', 'mobile', 'purchase']].astype(int)
 # A minusculas
-df['location'] = df['location'].str.lower()
+df['location']=df['location'].str.lower()
 
 # One-hot encoding
-df = pd.get_dummies(
-    data = df,
-    prefix = '',
-    prefix_sep = '',
-    columns = ['location'],
-    dtype = int
+df=pd.get_dummies(
+    data=df,
+    prefix='',
+    prefix_sep='',
+    columns=['location'],
+    dtype=int
 )
+
+# Interaction
+df['dark_mobile']=df['dark'].multiply(df['mobile'])
 
 # Constant
-df['const'] = 1
+df['const']=1
 
-# Declare linear model
-spec = sm.OLS(
-    endog = df['purchase'],
-    exog = df[['const', 'ireland','scotland','wales','dark']],
-    hasconst= True
+# Declare specification
+spec=sm.OLS(
+    endog=df['purchase'],
+    exog=df[['const', 'ireland', 'scotland', 'wales', 'dark', 'dark_mobile']],
+    hasconst=True
 )
 
-model = spec.fit()
+# Fit model
+model=spec.fit()
+
+# View results
+model.summary()
+
+# Declare model
+spec2=sm.OLS(
+    endog=df['purchase'], 
+    exog=df[['const', 'ireland', 'scotland', 'wales', 'dark']],  # No interaction
+    hasconst=True
+)
+
+# Fit model
+model2=spec2.fit()
+
+# View results
+model2.summary()
