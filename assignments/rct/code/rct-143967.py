a61305c0f1ba3811b8f27876cd8745b4384180d4
elisacamarena <elisa.camarena@hotmail.com>
2025-03-16 19:37:50 -0600

    tarea rct

--- assignments/rct/code/rct-143967.py
+++ assignments/rct/code/rct-143967.py
@@ -1,57 +1,23 @@
-# %% [markdown]
-# # Randomized Controlled Trials
-# 
-# We'll be taking a look at an online retailer based in the United Kingdom. Our
-# goal is to estimate the causal effect of switching the user's interface to dark
-# on the probability of purchasing an item.
-# 
-# We will fit the following model:
-# 
-# $$ E(Y_i | X_i) = X_i^T \gamma + \tau D_i$$
-# 
-# where $X_i$ are controls and $D_i$ indicates $i$'s treatment status.
-# 
-# ---
-# 
-# ## Imports
-
-# %%
+#Imports
 import os
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 
-# %% [markdown]
-# ## Exploratory Data Analysis
-# 
-# Load data
-
-# %%
+#Load data
 PATH = os.path.join('..', 'data', 'raw.csv')
 df = pd.read_csv(PATH)
 
-# %% [markdown]
-# View data
-
-# %%
+#View data
 df.head()
 
-# %% [markdown]
-# Check for nulls
-
-# %%
+#Check for nulls
 df.isna().sum()
 
-# %% [markdown]
-# Check data types
-
-# %%
+#Check datatypes
 df.dtypes
 
-# %% [markdown]
-# Clean data
-
-# %%
+#Clean data
 # Rename columns
 df.columns = ['id', 'dark', 'views', 'time', 'purchase', 'mobile', 'location']
 
@@ -72,13 +38,8 @@ df[['dark', 'mobile', 'purchase']] = df[['dark', 'mobile', 'purchase']].astype(i
 # Set `location`` to lowercase
 df['location'] = df['location'].str.lower()
 
-# %% [markdown]
-# Encode categorical variables to binary columns (also known as One-Hot Encoding)
-
-# %%
+#Encode categorical variables to binary columns (also known as One-Hot Encoding)
 df.head()
-
-# %%
 # One-hot encoding
 df = pd.get_dummies(
     data=df,
@@ -88,25 +49,14 @@ df = pd.get_dummies(
     dtype=int
 )
 
-# %% [markdown]
-# Feature engineering
-# 
-# - Create interaction term
-# - Assign a constant
-
-# %%
+#Create interaction term and assign a constant
 # Interaction
 df['dark_mobile'] = df['dark'].multiply(df['mobile'])
 
 # Constant
 df['const'] = 1
 
-# %% [markdown]
-# ## Fitting a linear model
-# 
-# Declare linear model
-
-# %%
+#Declare linear model
 # Declare specification
 spec = sm.OLS(
     endog=df['purchase'],
@@ -120,13 +70,7 @@ model = spec.fit()
 # View results
 model.summary()
 
-# %% [markdown]
-# - Interaction term is not significant (remove it)
-
-# %% [markdown]
-# Fitting a parsimonious model
-
-# %%
+#Fitting a parsimonious model
 # Declare model
 spec2 = sm.OLS(
     endog=df['purchase'],
@@ -139,5 +83,3 @@ model2 = spec2.fit()
 
 # View results
 model2.summary()
-
-
