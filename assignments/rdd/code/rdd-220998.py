1b7c4917b6faa50a1d4841f19f0edd9c0992c3e3
Lore Shaadi <Lore@MacBook-Pro-de-Lore.local>
2025-04-01 20:03:11 -0600

    intento

---
+++ assignments/rdd/code/rdd-220998.py
@@ -0,0 +1,21 @@
+# Equipo: Ainé Fernández, Eduardo Aguirre, Francisco Hernández, Lorena Shaadi. 
+
+import os
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+
+PATH = os.path.join('..', 'data', 'raw.csv')
+df = pd.read_csv(PATH)
+
+# Feature engineering
+CUTOFF = 40
+df['x'] = df['cohsize'] - CUTOFF
+
+# Declare and fit model
+spec = sm.OLS.from_formula(formula='avgverb ~ 1 + x + z + z * x', data=df)
+res = spec.fit(cov_type='HC3')
+
+# View results
+print(res.summary())
