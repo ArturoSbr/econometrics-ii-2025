df6f0c01b1f1022bc4448d3586e107ba6ccfeb54
David Vazquez Hernández <dvazquezh@mac-26891.local>
2025-04-08 20:48:19 -0600

    init tarea

---
+++ assignments/rdd/code/rdd-166427.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+import statsmodels.api as sm
+
+# Reading data
+PATH = os.path.join('..', 'data', 'raw.csv')
+df = pd.read_csv(PATH)
+
+#Seting the cutoff
+CUTOFF = 40
+df['x'] = df['cohsize'] - CUTOFF
+
+# Declaring and fit model
+spec = sm.OLS.from_formula(formula='avgverb ~ 1 + x + + z + z * x', data=df)
+res = spec.fit(cov_type='HC3')
