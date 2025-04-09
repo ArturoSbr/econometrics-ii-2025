eb7922fa9aba9ed5e96dc9d79f4c003bbfc0e0ea
joseangel <64157805+joseaalonso1@users.noreply.github.com>
2025-04-08 23:24:51 -0600

    V5 208404

--- assignments/ivs/code/ivs-208404.py
+++ assignments/ivs/code/ivs-208404.py
@@ -7,21 +7,13 @@ from linearmodels.iv import IV2SLS
 df = pd.read_csv('../data/raw.csv')
 df = df[df['yob'] >= 1940]
 yob_dummies = pd.get_dummies(df['yob'], prefix='yob')
-yob_dummies = yob_dummies.loc[:, [f'yob_{y}' for y in range(1940, 1950)]]
-
+yob_dummies = yob_dummies.loc[:, [f'yob_{y}' for y in range(1940, 1950)]].astype(int)
 qob_dummies = pd.get_dummies(df['qob'], prefix='qob')
-qob_dummies = qob_dummies.loc[:, [f'qob_{q}' for q in range(1, 5)]]
-
-yob_dummies = yob_dummies.astype(int)
-qob_dummies = qob_dummies.astype(int)
-
+qob_dummies = qob_dummies.loc[:, [f'qob_{q}' for q in range(1, 5)]].astype(int)
 df = pd.concat([df, yob_dummies, qob_dummies], axis=1)
-yob_cols = [f'yob_{y}' for y in range(1940, 1950)]
-qob_cols = [f'qob_{q}' for q in range(1, 5)]
-
 interaction_cols = {
     f'{y}_{q}': df[y] * df[q]
-    for y, q in itertools.product(yob_cols, qob_cols)
+    for y, q in itertools.product(yob_dummies.columns, qob_dummies.columns)
 }
 df = df.assign(**interaction_cols)
 formula = (
@@ -35,7 +27,8 @@ controls = [
     'race', 'married', 'smsa',
     'neweng', 'midatl', 'enocent', 'wnocent',
     'soatl', 'esocent', 'wsocent', 'mt'
-] + [f'yob_{y}' for y in range(1940, 1949)]  # exclude yob_1949
+] + [f'yob_{y}' for y in range(1940, 1949)]
+
 instr_vars = [
     f'yob_{y}_qob_{q}'
     for y in range(1940, 1950)
@@ -45,14 +38,11 @@ instr_vars = [
 endog = df['educ']
 exog = df[controls]
 instr = df[instr_vars]
-
 res1 = IV2SLS(
     dependent=df['lwklywge'],
     exog=sm.add_constant(exog, has_constant='add'),
     endog=endog,
     instruments=sm.add_constant(instr, has_constant='add')
 ).fit(cov_type='robust')
-
 bias = True
-
 bias_sign = '+'
