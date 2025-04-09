efdfc16f4b326c5c1e51009d62d5dc6605e599d6
Alfonso Martínez Contreras <73076053+Alfonso1478@users.noreply.github.com>
2025-04-08 19:13:59 -0600

    220512 RDD submission

---
+++ assignments/rdd/code/rdd-220512.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+import statsmodels.api as sm
+
+# Read data
+PATH = os.path.join('..', 'data', 'raw.csv')
+df = pd.read_csv(PATH)
+
+#Set the cutoff
+CUTOFF = 40
+df['x'] = df['cohsize'] - CUTOFF
+
+# Declare and fit model
+spec = sm.OLS.from_formula(formula='avgverb ~ 1 + x + + z + z * x', data=df)
+res = spec.fit(cov_type='HC3')
